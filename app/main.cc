91b551394856859395db885fb35dfe6af3a177bc
Michael Neumann <michael__neumann@t-online.de>
2024-07-28 23:16:25 +0200

    test

--- app/main.cc
+++ app/main.cc
@@ -27,6 +27,7 @@ int main(int argc, char **argv)
     std::cout << "\n\nUsage Example:\n";
 
     std::cout << "\n\nHallo Michael!!\n";
+    std::cout << "\n\nHallo Michael!!\n";
 
     // Compiler Warning and clang tidy error
     // std::int32_t i = 0;
